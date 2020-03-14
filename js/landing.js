7963663fc805e964cbdcb86fbff4ce5385c9c932
kishan <52867512+kumarkishan070997@users.noreply.github.com>
2020-03-14 23:36:12 +0530

    Update landing.js

--- js/landing.js
+++ js/landing.js
@@ -1,18 +1,18 @@
 anime({
-  targets: '.img1',
-  translateX: 250,
+  targets: '.sci.img1',
+  translateX: -250,
   endDelay: 1000,
   direction: 'alternate'
 });
 anime({
-  targets: '.img2',
-  translateX: 250,
+  targets: '.sci.img2',
+  translateX: -250,
   endDelay: 1000,
   direction: 'alternate'
 });
 anime({
-  targets: '.img3',
-  translateX: 250,
+  targets: '.sci.img3',
+  translateX: -250,
   endDelay: 1000,
   direction: 'alternate'
 });
